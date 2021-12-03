98100a5de735911ee048258ac7ed66a13a001e96
Keijiro Takahashi <keijiro@gmail.com>
2021-12-03 17:19:51 +0900

    Better label

--- Assets/PlayerFactory.cs
+++ Assets/PlayerFactory.cs
@@ -8,18 +8,23 @@ public static class PlayerFactory
     public static HapPlayer Create
       (string filePath, HapPlayer.PathMode pathMode)
     {
-        var go = new GameObject() { hideFlags = HideFlags.DontSave };
+        var go = new GameObject("[Not Saved] HAP Player for Bibcam");
+        go.hideFlags = HideFlags.DontSave;
+
         var hap = go.AddComponent<HapPlayer>();
         hap.Open(filePath, pathMode);
         hap.targetTexture = RenderTexture.GetTemporary(1920, 1080);
         hap.OnControlTimeStart();
+
         return hap;
     }
 
     public static void Destroy(HapPlayer instance)
     {
         if (instance == null) return;
+
         RenderTexture.ReleaseTemporary(instance.targetTexture);
+
         if (Application.isPlaying)
             Object.Destroy(instance.gameObject);
         else
