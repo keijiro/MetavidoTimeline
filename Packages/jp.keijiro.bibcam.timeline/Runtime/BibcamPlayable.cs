8ff28de158c526db184b732d828b84606d67702b
Keijiro Takahashi <keijiro@gmail.com>
2021-12-03 18:54:06 +0900

    Fix: Projection matrix corrupted

--- Packages/jp.keijiro.bibcam.timeline/Runtime/BibcamPlayable.cs
+++ Packages/jp.keijiro.bibcam.timeline/Runtime/BibcamPlayable.cs
@@ -56,6 +56,8 @@ public class BibcamPlayable : PlayableBehaviour
 
         camera.transform.localPosition = meta.CameraPosition;
         camera.transform.localRotation = meta.CameraRotation;
+
+        camera.ResetProjectionMatrix();
         camera.projectionMatrix =
           meta.ReconstructProjectionMatrix(camera.projectionMatrix);
     }
